03bcd36467f36e4485992f2834d751b2cc6834a7
Hai Nguyen <peternguyenkhac01@gmail.com>
2024-07-19 16:50:59 +0700

    init movie app

---
+++ Movie App/Data/MovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie_App.Models;
+
+namespace Movie_App.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ Movie App/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Movie_App.Data;
+
+namespace Movie_App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            // Add db context
+            builder.Services.AddDbContext<MovieContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("MovieApp")));
+
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                SeedData.Initialize(services);
+            }
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Movies}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
